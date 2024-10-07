7503b787a9df882f1f0855659619aad00912f572
choeunbin03 <choeunbiin03@naver.com>
2024-10-07 15:20:30 +0900

    Style type 생성

---
+++ src/types/signup.ts
@@ -0,0 +1,10 @@
+//export type BoardList = BoardListElement[]
+
+export type UserInfoElement = {
+  email: string;
+  //verification: string;
+  memberId: string;
+  nickName: string;
+  password: string;
+  //passwordConfirm: string;
+};
