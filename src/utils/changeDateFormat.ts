8de6d43c4e7c5934fe41e10df07cddf07c371e0b
hj7321 <oboki2001@naver.com>
2024-10-10 14:30:46 +0900

    [Feat] slice() 메서드 삭제

--- src/utils/changeDateFormat.ts
+++ src/utils/changeDateFormat.ts
@@ -1,7 +1,7 @@
-// Date 형식의 날짜(2024-10-07)를 24.10.07 형식으로 바꿔주는 함수
+// Date 형식의 날짜(2024-10-07)를 2024.10.07 형식으로 바꿔주는 함수
 
 export default function changeDateFormat(date: Date) {
-  const year = date.getFullYear().toString().slice(-2);
+  const year = date.getFullYear().toString();
   const month = (date.getMonth() + 1).toString().padStart(2, '0');
   const day = date.getDate().toString().padStart(2, '0');
 
