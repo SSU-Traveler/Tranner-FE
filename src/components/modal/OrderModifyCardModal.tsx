a8ec2e5ec629b70fe1d29514709f265c87f835cf
hj7321 <oboki2001@naver.com>
2024-10-10 14:25:40 +0900

    [Feat] 필요한 props 추가

--- src/components/modal/OrderModifyCardModal.tsx
+++ src/components/modal/OrderModifyCardModal.tsx
@@ -1,3 +1,5 @@
+import changeDateFormat from '../../utils/changeDateFormat';
+import getDayOfWeek from '../../utils/getDayOfWeek';
 import TripPlanCard from '../card/TripPlanCard';
 
 // OrderModifyCardModal 컴포넌트가 받게 될 props
@@ -5,14 +7,24 @@ import TripPlanCard from '../card/TripPlanCard';
 // 2. 여행 종료일
 // 3.
 
-export default function OrderModifyCardModal() {
+interface OrderModifyCardModalProps {
+  tripStartDate: Date;
+  tripEndDate: Date;
+}
+
+export default function OrderModifyCardModal({ tripStartDate, tripEndDate }: OrderModifyCardModalProps) {
+  const dayDiff: number = (tripEndDate.getTime() - tripStartDate.getTime()) / (1000 * 60 * 60 * 24);
+  const formattedTripStartDate: string = changeDateFormat(tripStartDate);
+  const formattedTripEndDate: string = changeDateFormat(tripEndDate);
+  const startDayOfWeek = getDayOfWeek(tripStartDate); // 시작하는 요일
+
   return (
     <div className="w-[500px] h-[650px] bg-white rounded-[12px] flex flex-col items-center p-[30px]">
       <p className="text-[20px] font-bold mb-[20px]">드래그해서 순서를 수정하세요</p>
       <section className="w-[440px] h-[500px] overflow-y-auto overflow-x-hidden mb-[20px] flex flex-col items-center scrollbar-custom">
         <div className="w-[410px] bg-button-selected text-white text-[14px] p-[5px] rounded-[5px] flex justify-between">
           <p>1일차 일정</p>
-          <p>2024.09.18 (수)</p>
+          <p>{formattedTripStartDate} (수)</p>
         </div>
         <section className="flex flex-col gap-[5px] py-[10px]">
           <div className="flex items-center gap-[15px]">
