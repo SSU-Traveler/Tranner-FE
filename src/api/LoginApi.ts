7b6200adb1aa4c308db30429f52fee4bf9be2b1f
choeunbin03 <choeunbiin03@naver.com>
2024-10-14 16:10:27 +0900

    Feat login 기능(자체 로그인, 카카오 로그인)

---
+++ src/api/LoginApi.ts
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import { UserInfoElement } from '../types/signup';
+const SIGNUP_API_BASE_URL = '/api';
+
+//로그인
+export const LoginApi = async (data: Omit<UserInfoElement, 'email' | 'nickName'>) => {
+  try {
+    const response = await axios.post(SIGNUP_API_BASE_URL + '/login', data);
+    console.log(response);
+  } catch (error) {
+    console.error('Error fetching boards:', error);
+    alert('로그인 문제');
+    return []; // 에러 발생 시 빈 배열 반환
+  }
+};
+
+//서버에 인가코드 넘겨주고 로그인.
+export const KakaoLogin = async (CODE: string) => {
+  try {
+    const response = await axios.post(
+      `${SIGNUP_API_BASE_URL}/kakaoLogin`,
+      { code: CODE },
+      { headers: { 'Content-Type': 'application/json;charset=utf-8' } }
+    );
+    const data = response.data;
+    return data;
+  } catch (error) {
+    console.error('Error fetching boards:', error);
+    return []; // 에러 발생 시 빈 배열 반환
+  }
+};
