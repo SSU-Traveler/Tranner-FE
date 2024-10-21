72d1de4545506ffa6857ce5ece3e20d0667dd67a
choeunbin03 <choeunbiin03@naver.com>
2024-10-21 15:13:18 +0900

    css 파일 import

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes/Router';
+import '../src/styles/date-range-picker.css';
 
 const queryClient = new QueryClient();
 
