310932695d54ea7c2df30373c56bedcf3033526f
hj7321 <oboki2001@naver.com>
2024-10-21 15:17:19 +0900

    [Design] 오버레이 스타일과 탑버튼 추가

--- src/shared/Layout.tsx
+++ src/shared/Layout.tsx
@@ -1,14 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import Footer from './Footer';
 import Header from './Header';
+import TopButton from './TopButton';
 
 export default function Layout() {
   return (
     <>
       <Header />
-      <main className="min-h-[900px] px-[120px]">
+      <main className="min-h-[900px] px-[120px] relative">
         <Outlet />
+        <div className="fixed inset-0 bg-black opacity-50 z-20 hidden" id="overlay"></div>
       </main>
+      <TopButton />
       <Footer />
     </>
   );
