f328a768ea54b6a643e97eb8f01e4ec5b35ea396
hj7321 <oboki2001@naver.com>
2024-10-21 15:16:07 +0900

    [Feat] 인터페이스 정의

--- src/components/input/PlaceInput.tsx
+++ src/components/input/PlaceInput.tsx
@@ -1,7 +1,11 @@
-export default function PlaceInput() {
+interface PlaceInputProp {
+  searchObj: string;
+}
+
+export default function PlaceInput({ searchObj }: PlaceInputProp) {
   return (
-    <div className="w-[750px] h-[60px] border border-black rounded-[4px] px-[16px] flex items-center justify-between">
-      <input type="text" placeholder="여행지를 검색해보세요!" />
+    <div className="w-[750px] h-[60px] border border-black bg-white rounded-[4px] px-[16px] flex items-center justify-between">
+      <input type="text" placeholder={`${searchObj}를 검색해보세요!`} className="w-[85%] outline-none" />
       <button>
         <img src="/search-glasses.svg" alt="검색" />
       </button>
