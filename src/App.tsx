fd894689732457ec64ccc5ac27a1edb14ad0ef51
hj7321 <oboki2001@naver.com>
2024-10-21 15:11:00 +0900

    [Style] ReactQueryDevtools 주석 처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes/Router';
 
@@ -9,7 +8,7 @@ export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-      <ReactQueryDevtools />
+      {/* <ReactQueryDevtools /> */}
     </QueryClientProvider>
   );
 }
