75ac134132cf2fb973388843ae6996f3fd769e7f
choeunbin03 <choeunbiin03@naver.com>
2024-11-01 11:02:46 +0900

    feat 캘린더 모달로 띄우기 위한 모달 컨테이너

---
+++ src/components/modal/ModalContainer.tsx
@@ -0,0 +1,12 @@
+import React, { ReactNode } from 'react';
+import { createPortal } from 'react-dom';
+
+interface Props {
+  children: ReactNode;
+}
+
+function ModalContainer({ children }: Props) {
+  return createPortal(<>{children}</>, document.getElementById('modal')!);
+}
+
+export default ModalContainer;
