8719e0dcbfa19a0a89ac0f7a25c165950ae7a12c
choeunbin03 <choeunbiin03@naver.com>
2024-11-18 15:59:39 +0900

    style 의미 없

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -172,12 +172,11 @@ export default function MainPage() {
               needToLoginAlarm={needToLoginAlarm}
             />
             <SpotCard
-              imgPath="/images/example-lotteworld.jpg"
-              spotName="롯데월드"
-              spotEngName="Lotte World"
-              spotAddress="서울특별시 송파구 올림픽로 240"
-              spotDescription="롯데월드(영어: Lotte World)는 대한민국 서울특별시 송파구 올림픽로 240에 위치한 테마파크이다. 롯데그룹의 계열사인 호텔롯데 월드 사업부에서 운영한다.
-              놀이시설은 실내의 롯데월드 어드벤처(Lotte World Adventure)와 야외의 매직아일랜드가 운영되고 있으며, 민속박물관, 아이스링크, 백화점, 마트, 호텔 등이 포함된다."
+              imgPath="/images/example-gwanghwamun.jpg"
+              spotName="광화문"
+              spotEngName="Gwanghwamun"
+              spotAddress="서울특별시 종로구 사직로 161"
+              spotDescription="              광화문 앞에는 2023년에 복원한 월대가 자리잡고 있으며, 양쪽에는 한 쌍의 해태 조각상이 자리잡고 있다. 광화문의 석축부에는 세 개의 홍예문(虹霓門, 아치문)이 있다. 가운데 문은 임금이 다니던 문이고, 나머지 좌우의 문은 신하들이 다니던 문이었는데, 왼쪽 문은 무신이, 오른쪽 문은 문신이 출입했다. 광화문의 가운데 문 천장에는 주작이 그려져 있고, 왼쪽 문에는 거북이가, 오른쪽 문에는 천마가 그려져 있다."
               needToLoginAlarm={needToLoginAlarm}
             />
             <SpotCard
