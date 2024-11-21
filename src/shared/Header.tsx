57a5de1f603ae8b19239761d9e5f0cbdd7d22119
hj7321 <oboki2001@naver.com>
2024-11-21 14:13:20 +0900

    [Style] 헤더 내용물 튀어나오지 않게 수정

--- src/shared/Header.tsx
+++ src/shared/Header.tsx
@@ -1,27 +1,37 @@
+import clsx from 'clsx';
 import { Link, useLocation } from 'react-router-dom';
 
-const tabStyle = 'hover:font-bold hover:text-button-selected';
-const clickedTabStyle = 'font-bold text-button-selected';
+const tabStyle = 'hover:font-bold hover:text-button-hover';
+const selectedTabStyle = 'font-bold text-button-selected';
 const buttonStyle =
-  'w-[144px] h-[40px] bg-button-basic rounded-[8px] text-white flex justify-center items-center font-bold hover:bg-button-hover';
+  'lg:w-[144px] w-[100px] h-[40px] bg-button-basic rounded-[8px] text-white flex justify-center items-center font-bold hover:bg-button-hover';
 
 export default function Header() {
   const { pathname }: { pathname: string } = useLocation();
 
   return (
-    <header className="h-[70px] border border-b-[#495057] px-[120px] flex justify-between">
-      <div className="flex justify-center items-center gap-[50px]">
-        <Link to="/" className="text-[32px] font-extrabold">
-          Tranner
+    <header className="min-w-[900px] h-[70px] border border-b-[#495057] px-[120px] flex justify-between">
+      <div className="flex justify-center items-center lg:gap-[50px] md:gap-[40px]">
+        <Link to="/" className="text-[32px] font-extrabold hover:text-button-hover whitespace-nowrap">
+          🚄Tranner🏕️
         </Link>
         <nav className="flex gap-[20px]">
-          <Link to="/local-view" className={pathname === '/local-view' ? clickedTabStyle : tabStyle}>
+          <Link
+            to="/local-view"
+            className={clsx(pathname === '/local-view' ? selectedTabStyle : tabStyle, 'whitespace-nowrap')}
+          >
             일정 짜기
           </Link>
-          <Link to="/weather-view" className={pathname === '/weather-view' ? clickedTabStyle : tabStyle}>
+          <Link
+            to="/weather-view"
+            className={clsx(pathname === '/weather-view' ? selectedTabStyle : tabStyle, 'whitespace-nowrap')}
+          >
             날씨 보기
           </Link>
-          <Link to="/custom-trip" className={pathname === '/custom-trip' ? clickedTabStyle : tabStyle}>
+          <Link
+            to="/custom-trip"
+            className={clsx(pathname === '/custom-trip' ? selectedTabStyle : tabStyle, 'whitespace-nowrap')}
+          >
             맞춤 여행지
           </Link>
         </nav>
