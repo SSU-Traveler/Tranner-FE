73c30f2fa95f9b9b9d41b4492ed144ee9d007bb5
hj7321 <oboki2001@naver.com>
2024-11-24 10:42:31 +0900

    [Add] 날씨 관련 타입 추가

--- src/types/weather.type.ts
+++ src/types/weather.type.ts
@@ -1,31 +1,35 @@
+export type Weather = {
+  description: string; // 그룹 내 날씨 상태
+  icon: string; // 날씨 아이콘 아이디
+  id: number; // 기상 조건 ID
+  main: string; // 날씨 매개변수 그룹(비, 눈, 구름 등)
+};
+
+export type Main = {
+  feels_like: number; // 체감 온도 (℃)
+  grnd_level: number; // 지상의 대기압 (hPa)
+  humidity: number; // 습도 (%)
+  pressure: number; // 기본적으로 해수면의 대기압 (hPa)
+  sea_level: number; // 기본적으로 해수면의 대기압 (hPa)
+  temp: number; // 온도 (℃)
+  temp_kf?: number;
+  temp_max?: number; // 최고 기온, 최대 예측 온도(대도시 및 도시 지역 내) (℃)
+  temp_min?: number; // 최저 기온, 최소 예측 온도(대도시 및 도시 지역 내) (℃)
+};
+
 export type FutureWeatherList = {
   clouds: {
     all: number; // 흐림 정도 (%)
   };
   dt: number; // 데이터 예측 시간 (Unix, UTC) *한국 시간으로 변환 필요!
   dt_txt: string; // 데이터 예측 시간 (ISO, UTC) *한국 시간으로 변환 필요!
-  main: {
-    feels_like: number; // 체감 온도 (℃)
-    grnd_level: number; // 지상의 대기압 (hPa)
-    humidity: number; // 습도 (%)
-    pressure: number; // 기본적으로 해수면의 대기압 (hPa)
-    sea_level: number; // 기본적으로 해수면의 대기압 (hPa)
-    temp: number; // 온도 (℃)
-    temp_kf: number;
-    temp_max?: number; // 최고 기온, 최대 예측 온도(대도시 및 도시 지역 내) (℃)
-    temp_min?: number; // 최저 기온, 최소 예측 온도(대도시 및 도시 지역 내) (℃)
-  };
+  main: Main;
   pop: number; // 강수 확률 (0에서 1사이, 0은 0%, 1은 100%)
   sys: {
     pod: string; // 하루 중 일부 (n은 밤, d는 낮)
   };
   visibility: number; // 평균 가시거리 (meter, 가시거리의 최대값은 10km)
-  weather: {
-    description: string; // 그룹 내 날씨 상태
-    icon: string; // 날씨 아이콘 아이디
-    id: number; // 기상 조건 ID
-    main: string; // 날씨 매개변수 그룹(비, 눈, 구름 등)
-  }[];
+  weather: Weather[];
   wind: {
     speed: number; // 풍속 (m/s)
     deg: number; // 풍향, 각도(기상)
@@ -51,4 +55,41 @@ export type FutureWeather = {
   cod: string;
   list: FutureWeatherList[];
   message: number;
+};
+
+export type CurrentWeather = {
+  base: string;
+  clouds: {
+    all: number;
+  };
+  cod: number;
+  coord: {
+    lat: number;
+    lon: number;
+  };
+  dt: number;
+  id: number;
+  main: Main;
+  name: string;
+  sys: {
+    country: string;
+    id: number;
+    sunrise: number;
+    sunset: number;
+    type: number;
+  };
+  timezone: number;
+  visibility: number;
+  weather: Weather[];
+  wind: {
+    deg: number;
+    speed: number;
+    gust?: number;
+  };
+  rain?: {
+    '1h': number; // (사용 가능한 경우)강수량 (mm/h)
+  };
+  snow?: {
+    '1h': number; // (사용 가능한 경우)강설량 (mm/h)
+  };
 };
