8911b2a99caf01b164138945cd3bb736aa8b9eb4
hj7321 <oboki2001@naver.com>
2024-11-24 10:26:08 +0900

    [Feat] 오늘과 미래를 구분하기 위한 로직 추가, key 속성 추가

--- src/components/weather/TableBody.tsx
+++ src/components/weather/TableBody.tsx
@@ -5,12 +5,24 @@ import getWindDirection from '../../utils/getWindDirection';
 const tableStyle = 'border border-gray-300 p-[10px] text-center text-[18px]';
 const TimeInterval = ['00:00', '03:00', '06:00', '09:00', '12:00', '15:00', '18:00', '21:00'];
 
-export default function TableBody({ weathers }: { weathers: FutureWeatherList[] }) {
+interface TableBodyProps {
+  weathers: FutureWeatherList[];
+  isToday: boolean;
+}
+
+export default function TableBody({ weathers, isToday }: TableBodyProps) {
+  let length: number = 0;
+  let newTimeInterval: string[] = [];
+  if (isToday && weathers) {
+    length = weathers.length;
+    newTimeInterval = [...TimeInterval].reverse().slice(0, length).reverse();
+  }
+
   return (
     <tbody>
-      {weathers.map((weather, index) => (
-        <tr>
-          <td className={tableStyle}>{TimeInterval[index]}</td>
+      {weathers?.map((weather, index) => (
+        <tr key={index}>
+          <td className={tableStyle}>{isToday ? newTimeInterval[index] : TimeInterval[index]}</td>
           <td className={tableStyle}>{weather.main.temp}℃</td>
           <td className={tableStyle}>{weather.main.feels_like}℃</td>
           <td className={tableStyle}>{weather.main.humidity}%</td>
@@ -19,10 +31,9 @@ export default function TableBody({ weathers }: { weathers: FutureWeatherList[] 
           <td className={tableStyle}>
             {weather.pop >= 0.3 && weather.pop < 0.5 && '☂️ '}
             {weather.pop >= 0.5 && '☔ '}
-            {weather.pop * 100}%
+            {Math.round(weather.pop * 100)}%
           </td>
           <td className={tableStyle}>
-            {weather.wind.speed >= 6 && weather.wind.speed < 11 && '💨 '}
             {weather.wind.speed >= 11 && '🌀 '}
             {weather.wind.speed}m/s
           </td>
