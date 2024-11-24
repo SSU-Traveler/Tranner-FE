4578825e575a6e0f96ec1ae8a125f310863d9a9a
hj7321 <oboki2001@naver.com>
2024-11-24 10:27:10 +0900

    [Add] 받는 props 추가

--- src/components/weather/Table.tsx
+++ src/components/weather/Table.tsx
@@ -1,19 +1,22 @@
 import { FutureWeatherList } from '../../types/weather.type';
+import getDateAndDay from '../../utils/getDateAndDay';
 import TableBody from './TableBody';
 import TableHeader from './TableHeader';
 
-const tableStyle = 'w-[85%] border border-gray-300 p-[10px] text-center';
-const divStyle = 'mb-[30px] flex justify-center p-[10px]';
-const textStyle = 'text-[20px] text-center';
+interface TableProps {
+  weather: FutureWeatherList[];
+  index?: number;
+  isToday: boolean;
+}
 
-export default function Table({ weather }: { weather: FutureWeatherList[] }) {
+export default function Table({ weather, index, isToday }: TableProps) {
   return (
     <>
-      <div className={textStyle}>11.21.(목) 수정 필요!</div>
-      <div className={divStyle}>
-        <table className={tableStyle}>
+      {!isToday && <div className="text-[20px] text-center">{getDateAndDay(index! + 1)}</div>}
+      <div className="mb-[30px] flex justify-center p-[10px]">
+        <table className="w-[85%] border border-gray-300 p-[10px] text-center">
           <TableHeader />
-          <TableBody weathers={weather} />
+          <TableBody weathers={weather} isToday={isToday} />
         </table>
       </div>
     </>
