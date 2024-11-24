5e6e61cefae35ab7d253f7f43c1bc6f3ee61eb71
hj7321 <oboki2001@naver.com>
2024-11-24 14:45:56 +0900

    [Chore] 바뀐 내용 없음

--- src/components/card/SurveyResultCard.tsx
+++ src/components/card/SurveyResultCard.tsx
@@ -1,22 +1,39 @@
+import { useState } from 'react';
+
 export default function SurveyResultCard() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
   const truncateWord = (placeName: string) => {
-    if (placeName.length > 13) {
-      return placeName.slice(0, 12) + '...';
+    if (placeName.length > 14) {
+      return placeName.slice(0, 13) + '...';
     }
     return placeName;
   };
 
   return (
-    <div
-      className="w-[220px] h-[264px] bg-white rounded-[16px] border border-[#B2B9C0] hover:cursor-pointer flex flex-col gap-[5px] items-center p-[10px]"
-      // onClick={openCardModal}
-    >
-      <img
-        src="/images/place/서울.jpg"
-        alt="서울 롯데타워"
-        className="w-[200px] h-[220px] rounded-[16px] object-cover"
-      />
-      <p className="text-[#495057] font-bold text-[15px] self-start">{truncateWord('서울 롯데타워')}</p>
-    </div>
+    <>
+      <div
+        className="w-[220px] h-[264px] bg-white rounded-[16px] border border-[#B2B9C0] hover:cursor-pointer flex flex-col gap-[5px] items-center p-[10px]"
+        onClick={() => setIsOpen(true)}
+      >
+        <img
+          src="/images/place/서울.jpg"
+          alt="서울 롯데타워"
+          className="w-[200px] h-[220px] rounded-[16px] object-cover"
+        />
+        <p className="text-[#495057] font-bold text-[15px] self-start">{truncateWord('서울 롯데타워')}</p>
+      </div>
+      {/* {isOpen && (
+        <ModalContainer>
+          <CardModal
+            imgPath="/images/place/경기.jpg"
+            placeKorName="수원 화성"
+            placeAddress="주소"
+            placeDescription="설명"
+            // needToLoginAlarm={needToLoginAlarm}
+          />
+        </ModalContainer>
+      )} */}
+    </>
   );
 }
