ac2ef22348b30fab858bd70cce606dfe40b1ea92
hj7321 <oboki2001@naver.com>
2024-12-07 01:42:33 +0900

    [Chore] 주석 처리 및 주석 해제

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router-dom';
 import { ModalProvider } from './contexts/modal.context';
 import router from './routes/Router';
@@ -10,7 +11,7 @@ export default function App() {
     <QueryClientProvider client={queryClient}>
       <ModalProvider>
         <RouterProvider router={router} />
-        {/* <ReactQueryDevtools /> */}
+        <ReactQueryDevtools />
       </ModalProvider>
     </QueryClientProvider>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel/slick/slick.css';
+import '../src/styles/custom-scrollbar.css';
+import '../src/styles/date-range-picker.css';
 import App from './App.tsx';
 import './index.css';
-import '../src/styles/date-range-picker.css';
-import '../src/styles/custom-scrollbar.css';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  // <StrictMode>
+  <App />
+  // </StrictMode>
 );
