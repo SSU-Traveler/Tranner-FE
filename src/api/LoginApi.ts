a65af8a6fcba9b521bf79661e824dca49d96faa9
choeunbin03 <choeunbiin03@naver.com>
2024-12-08 21:07:03 +0900

    last commit

--- src/api/LoginApi.ts
+++ src/api/LoginApi.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosResponse } from 'axios';
 import { UserInfoElement } from '../types/signup.type';
-//const SIGNUP_API_BASE_URL = import.meta.env.VITE_SERVER_URL;
+const SIGNUP_API_BASE_URL = import.meta.env.VITE_SERVER_URL;
 //const SIGNUP_API_BASE_URL = 'https://api.tranner.com';
-const SIGNUP_API_BASE_URL = 'http://localhost:8080';
+//const SIGNUP_API_BASE_URL = 'http://localhost:8080';
 
 //로그인
 export const LoginApi = async (data: Omit<UserInfoElement, 'email' | 'nickname'>): Promise<AxiosResponse | null> => {
--- src/api/SignUpApi.ts
+++ src/api/SignUpApi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { UserInfoElement } from '../types/signup.type';
-//const SIGNUP_API_BASE_URL = '/api/member';
-const SIGNUP_API_BASE_URL = 'http://localhost:8080/member';
+const SIGNUP_API_BASE_URL = '/api/member';
+//const SIGNUP_API_BASE_URL = 'http://localhost:8080/member';
 
 //회원 가입
 export const SignUpApi = async (prop: UserInfoElement) => {
